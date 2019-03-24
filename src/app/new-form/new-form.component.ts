3669b678db03eed5cf8cb05c1a2517b35d23bd0d
Daneshwaran <daneshwaranm@gmail.com>
2019-03-24 22:03:57 +0530

    latest

--- src/app/new-form/new-form.component.ts
+++ src/app/new-form/new-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
 import regExpValidator from '../custom-validators/regular-expression';
 
 @Component({
@@ -10,9 +10,21 @@ import regExpValidator from '../custom-validators/regular-expression';
 export class NewFormComponent implements OnInit {
   email = new FormControl('', [Validators.required, Validators.email]);
   nokiaId = new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(6), regExpValidator(/[A-Z]/, "capital"), regExpValidator(/[a-z]/, "small"), regExpValidator(/[!@#\`\$%\^\&*\)\(+=._-]/, "special")]);
-  constructor() { }
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+  thirdFormGroup: FormGroup;
+  constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
+    this.thirdFormGroup = this._formBuilder.group({
+      thirdCtrl: ['', Validators.required]
+    });
   }
   getErrorMessage() {
     return this.email.hasError('required') ? 'You must enter a value' :
